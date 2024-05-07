1a380c41676b4ef8623ed044e0be7c3759ee3b57
loculy <loculy@student.42mulhouse.fr>
2024-05-07 15:18:26 +0200

    init

---
+++ AllClass.py
@@ -0,0 +1,98 @@
+import base64
+import json
+from datetime import datetime, timedelta
+
+import requests
+from amqp import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.serialization import load_pem_private_key
+from cryptography.hazmat.primitives import serialization
+from datetime import datetime, timezone
+import random
+
+BLOCKCHAIN_URL = "http://k1r4p6"
+BLOCKCHAIN_HOST = "8020"
+ROOMMANAGER_URL = "http://localhost"
+ROOMMANAGER_HOST = "8000"
+
+
+class Uniqid:
+
+    @staticmethod
+    def __getRandom():
+        random_number = random.randint(0, 99)
+        formatted_number = str(random_number).zfill(2)
+        return formatted_number
+
+    @staticmethod
+    def getUnixTimeStamp():
+        now_utc = datetime.now(timezone.utc)
+        unix_timestamp = int(now_utc.timestamp())
+        return unix_timestamp
+
+    @staticmethod
+    def generate():
+        uniqid = Uniqid.__getRandom() + Uniqid.__getRandom()
+        uniqid += str(Uniqid.getUnixTimeStamp())
+        uniqid += Uniqid.__getRandom() + Uniqid.__getRandom()
+        return uniqid
+
+
+class Signature:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def getPrivateKey():
+        f = open("private_key.pem", "rb")
+        load_key = load_pem_private_key(f.read(), None)
+        return load_key
+
+    @staticmethod
+    def create_signed_token(data, life_span_minutes=5):
+        private_key = Signature.getPrivateKey()
+
+        data['transaction_id'] = Uniqid.generate()
+        data['expires'] = (datetime.utcnow() + timedelta(minutes=life_span_minutes)).isoformat()
+        data_json = json.dumps(data)
+
+        data_bytes = data_json.encode('utf-8')
+        signature = private_key.sign(
+            data_bytes,
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+        signature_encoded = base64.b64encode(signature).decode('utf-8')
+        return data, signature_encoded
+
+
+def postMatch_ToBC(match):
+    print("[INPUT] match ", match)
+    try:
+        data, signature = Signature.create_signed_token(match)
+
+        headers = {"Authorization": str(signature)}
+        host = BLOCKCHAIN_URL + ":" + BLOCKCHAIN_HOST
+        url = host + '/match/post/'
+        x = requests.post(url, json=data, headers=headers)
+        print("[SUCCESS] Post ", x)
+    except Exception as e:
+        print("[ERROR] Post ", e)
+
+
+def postMatch_ToRM(match):
+    print("[INPUT] match ", match)
+    try:
+        data, signature = Signature.create_signed_token(match)
+
+        headers = {"Authorization": str(signature)}
+        host = ROOMMANAGER_URL + ":" + ROOMMANAGER_HOST
+        url = host + '/api/match_result/'
+        x = requests.post(url, json=data, headers=headers)
+        print("[SUCCESS] Post ", x)
+    except Exception as e:
+        print("[ERROR] Post ", e)
---
+++ main.py
@@ -0,0 +1,44 @@
+# ======== MATCH TO BLOCKCHAIN ==========
+from AllClass import Uniqid, postMatch_ToRM
+
+
+def match_ToBC(match_id):
+    data_json_raw = {
+        "match_id": match_id,
+        "tournament_id": 0,
+        "player1_score": 0,
+        "player2_score": 10,
+        "player1_id": 12348546545,
+        "player2_id": 12348546545,
+        "winner_id": 12348546545,
+        "timestamp": 12348546545
+    }
+    return data_json_raw
+
+
+#postMatch_ToBC(match_ToBC(1035))
+#postMatch_ToBC(match_ToBC(1036))
+#postMatch_ToBC(match_ToBC(1037))
+#postMatch_ToBC(match_ToBC(1037))
+#postMatch_ToBC(match_ToBC(1038))
+
+
+# ======== MATCH RESULT TO ROOM MANAGER ==========
+def match_ToRM(id_match, p_a, p_b):
+    data_json_raw = {
+        "match_id": id_match,
+        "tournament_id": 0,
+        "player1_score": 0,
+        "player2_score": 10,
+        "player1_id": p_a,
+        "player2_id": p_b,
+        "winner_id": p_b,
+    }
+    return data_json_raw
+
+
+id_already_exist = Uniqid.generate()
+postMatch_ToRM(match_ToRM(id_already_exist, Uniqid.generate(), Uniqid.generate()))
+postMatch_ToRM(match_ToRM(id_already_exist, Uniqid.generate(), Uniqid.generate()))
+postMatch_ToRM(match_ToRM(Uniqid.generate(), Uniqid.generate(), Uniqid.generate()))
+postMatch_ToRM(match_ToRM(Uniqid.generate(), Uniqid.generate(), Uniqid.generate()))
